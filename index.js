e49dd9f9fe4bf82db2ee4415bd53862cfa3ca7cd
omarbassatni <omarbassatni@gmail.com>
2022-10-15 15:11:28 +0300

    finished

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
   //add static methods here
+  static capitalize(word) {
+    word.charAt(0).toUpperCase()
+    return word
+  }
+  static sanitize(string) {
+    const splittedString = string.split(' ')
+    splittedString.map(word => {
+      const sanitizedWord = word.split('')
+      .filter((char) => typeof char == 'string' || typeof char == 'number')
+      return sanitizedWord.join('')
+    })
+  }
 }
+
